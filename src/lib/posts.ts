ae26e7375cee8fc8b40a58df9be8c8201fe546bf
arxisme <arsh11100arsh@gmail.com>
2025-04-10 08:04:09 +0530

    test

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -2,18 +2,18 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
-const contentDir = path.join(process.cwd(),  'content');
+const contentDir = path.join(process.cwd(),  'src/content');
 type PostMeta = {
   slug: string;
   title: string;
   date: string;
 };
 export function getAllPosts(): PostMeta[] {
-  const files = fs.readdirSync(path.join(process.cwd(),'content'));
+  const files = fs.readdirSync(path.join(process.cwd(),'src/content'));
 
   return files.map((file) => {
     const slug = file.replace('.md', '');
-    const raw = fs.readFileSync(path.join(path.join(process.cwd(),'content'), file), 'utf-8');
+    const raw = fs.readFileSync(path.join(path.join(process.cwd(),'src/content'), file), 'utf-8');
     const { data } = matter(raw);
 
     return {
@@ -30,3 +30,4 @@ export function getPost(slug: string) {
   const { data, content } = matter(raw);
   return { ...data, content };
 }
+
